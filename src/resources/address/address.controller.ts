024e89126269286efb20777a90fe6029bbd30894
NMakarevich <nikoladze529@mail.ru>
2025-10-22 16:43:11 +0300

    refactor: add role permission for delete method

--- src/resources/address/address.controller.ts
+++ src/resources/address/address.controller.ts
@@ -1,7 +1,9 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
 import { AddressService } from './address.service';
 import { CreateAddressDto } from './dto/create-address.dto';
 import { UpdateAddressDto } from './dto/update-address.dto';
+import { Roles } from '../../decorators/roles.decorator';
+import { ENGINEER_ROLE } from '../engineer/entities/engineer.entity';
 
 @Controller('address')
 export class AddressController {
@@ -27,6 +29,7 @@ export class AddressController {
     return this.addressService.update(id, updateAddressDto);
   }
 
+  @Roles(ENGINEER_ROLE.ADMIN)
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.addressService.remove(id);
