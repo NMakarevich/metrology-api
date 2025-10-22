6dc40f4525e0bdf8e62e7bcf9b7e9c9817be5184
NMakarevich <nikoladze529@mail.ru>
2025-10-22 17:03:10 +0300

    fix: enable global validation for dto

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import 'dotenv/config';
 import * as process from 'node:process';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
   const port = Number(process.env.PORT ?? 4100);
 
   await app.listen(port);
