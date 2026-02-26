5740db26b02aeff1d48da19c88d436ed437d2b67
NMakarevich <nikoladze529@mail.ru>
2026-02-26 22:48:53 +0300

    feat: add registry endpoint

--- src/resources/auth/auth.controller.ts
+++ src/resources/auth/auth.controller.ts
@@ -1,17 +1,29 @@
-import { Controller, Post, UseGuards, Request, HttpCode, HttpStatus } from '@nestjs/common';
+import { Controller, Post, UseGuards, Request, HttpCode, HttpStatus, Body } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LocalAuthGuard } from './local-auth.guard';
 import { Public } from '../../decorators/public.decorator';
+import { CreateEngineerDto } from '../engineer/dto/create-engineer.dto';
+import { EngineerService } from '../engineer/engineer.service';
 
+@Public()
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(
+    private readonly authService: AuthService,
+    private readonly engineerService: EngineerService,
+  ) {}
 
-  @Public()
   @UseGuards(LocalAuthGuard)
   @HttpCode(HttpStatus.OK)
   @Post('/login')
   async login(@Request() req) {
     return this.authService.login(req.user);
+  }
+
+  @UseGuards(LocalAuthGuard)
+  @HttpCode(HttpStatus.CREATED)
+  @Post('/registry')
+  async registry(@Body() createEngineerDto: CreateEngineerDto) {
+    return this.engineerService.create(createEngineerDto);
   }
 }
