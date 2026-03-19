b7be1ffdba00ff53683c7e500476022af57389c2
NMakarevich <nikoladze529@mail.ru>
2026-03-19 12:34:14 +0300

    feat: add HttpExceptionFilter

--- src/main.ts
+++ src/main.ts
@@ -4,13 +4,14 @@ import 'dotenv/config';
 import * as process from 'node:process';
 import { ValidationPipe } from '@nestjs/common';
 import { PrismaExceptionFilter } from './filters/prisma-exception.filter';
+import { HttpExceptionFilter } from './filters/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   const port = Number(process.env.PORT ?? 4100);
 
-  app.useGlobalFilters(new PrismaExceptionFilter());
+  app.useGlobalFilters(new PrismaExceptionFilter(), new HttpExceptionFilter());
   await app.listen(port);
 }
 bootstrap();
