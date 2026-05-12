3ca204a5373c68c6f02106506a5920edb00a9323
NMakarevich <nikoladze529@mail.ru>
2026-05-12 17:25:36 +0300

    refactor: delete mock db

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,7 +4,6 @@ import { AppService } from './app.service';
 import { EngineerModule } from './resources/engineer/engineer.module';
 import { RolesGuard } from './guards/roles.guard';
 import { APP_GUARD } from '@nestjs/core';
-import { EngineersDB } from './mock/engineers';
 import { AuthModule } from './resources/auth/auth.module';
 import { AddressModule } from './resources/address/address.module';
 import { ClinicModule } from './resources/clinic/clinic.module';
@@ -40,7 +39,6 @@ import { PlanningModule } from './resources/planning/planning.module';
       provide: APP_GUARD,
       useClass: RolesGuard,
     },
-    EngineersDB,
     JwtService,
   ],
 })
